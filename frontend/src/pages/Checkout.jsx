74fcd7d3819a34134a2c6ee585a278be850383dc
naemaalrahbi <16s1983@utas.edu.om>
2026-05-18 03:51:48 +0400

    Update Checkout.jsx

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -41,7 +41,7 @@ function Checkout() {
             orderStatus: "pending",
         };
 
-        const res = await axios.post("http://localhost:5001/order/create", orderData);
+        const res = await axios.post("https://smartpharmacy-lysm.onrender.com/order/create", orderData);
         const fullOrder = {
             ...res.data.order,
             items: items.map((i) => ({ productId: i.productId, quantity: i.quantity })),
